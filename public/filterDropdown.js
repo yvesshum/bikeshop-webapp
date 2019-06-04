725740d27efbc95da6b970e3c61d375c49a976f8
yvesshum <yvesshum1210@gmail.com>
2019-06-03 19:26:31 -0500

    progress on abstracting filter function

--- public/filterDropdown.js
+++ public/filterDropdown.js
@@ -14,7 +14,7 @@ class filterDropdown {
 
     // data from firestore based on requested period
     async getData() {
-        console.log('hey');
+
         // var query = await this.firebaseRef.where("ActivePeriods", "array-contains", "spring19").get();
         var query = await this.firebaseRef.where(this.queryField, this.queryOperator, this.lookupValue).get();
         var records = query.docs.map(doc => {
@@ -22,9 +22,26 @@ class filterDropdown {
             ret["ID"] = doc.id;
             return ret;
         });
+        console.log(records);
         this.data = records;
     }
 
+
+    filterFunction() {
+        let input, filter, ul, li, a, i;
+        input = document.getElementById(this.targetDiv + " myInput");
+        filter = input.value.toUpperCase();
+        let div = document.getElementById(this.targetDiv + " myDropdown");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+            let txtValue = a[i].textContent || a[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
+            } else {
+                a[i].style.display = "none";
+            }
+        }
+    }
     //this builds the text to be displayed no the dropdown items
     buildText(object, fields) {
         var ret = "";
@@ -35,42 +52,109 @@ class filterDropdown {
         return ret
     }
 
+
     buildForm() {
         if (this.data === null) {
             console.error("no data found, please call getData() first.")
         }
         else {
-            for (var i = 0; i < this.data.length; i ++) {
+
+            var formDiv = document.createElement('div');
+            formDiv.id = this.targetDiv + " myDropdown" ;
+            formDiv.setAttribute('class', "dropdown-content");
+
+            var input = document.createElement('input');
+            input.type = 'text';
+            input.placeholder = "Search..";
+            input.id = this.targetDiv + " myInput";
+
+
+            formDiv.appendChild(input);
+            document.getElementById(this.targetDiv).appendChild(formDiv);
+            document.getElementById(this.targetDiv + " myInput").onkeyup = this.filterFunction;
+
+            var button = document.createElement('button');
+            button.innerHTML = "Dropdown";
+            var that = this;
+            button.addEventListener("click", function () {
+                document.getElementById(that.targetDiv + " myDropdown").classList.toggle("show");
+            });
+            button.setAttribute('class', 'dropbtn');
+
+
+            document.getElementById(this.targetDiv).append(button);
+
+
+            for (var i = 0; i < this.data.length; i++) {
                 var element = this.data[i];
                 var node = document.createElement('a');
                 var nodeText = document.createTextNode(this.buildText(element, this.elementFields));
                 node.appendChild(nodeText);
                 node.title = element.ID;
                 node.href = element.ID;
-                document.getElementById(this.targetDiv).appendChild(node);
+                document.getElementById(this.targetDiv + " myDropdown").appendChild(node);
             }
 
         }
     }
 
-
-
-    // async function getDocs() {
-    //     var snapshot =  await collectionRef.get();
-    //     recordsThing = snapshot.docs.map(doc => doc.data());
-    //     console.log(recordsThing);
-    //     // console.log(recordsThing[0]["Make"]);
-    //     recordsThing.forEach(function(element) {
-    //         console.log(element["Make"]);
-    //         var node = document.createElement('a');
-    //         var nodeText = document.createTextNode(element["Make"]);
-    //         node.appendChild(nodeText);
-    //         node.title = element["Make"];
-    //         node.href = element["Make"];
-    //         document.getElementById("myDropdown").appendChild(node);
-    //     });
-    //     // console.log('s: ', snapshot.docs.map(doc => doc.data()));
-    // }
-
 }
+/*
+you might want to use this styling
+<style>
+    .dropbtn {
+      background-color: #262626;
+      color: white;
+      padding: 16px;
+      font-size: 16px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .dropbtn:hover, .dropbtn:focus {
+      background-color: #454745;
+    }
+
+    #myInput {
+      border-box: box-sizing;
+      background-image: url('search_icon.png');
+      background-position: 14px 12px;
+      background-repeat: no-repeat;
+      font-size: 16px;
+      padding: 14px 20px 12px 45px;
+      border: none;
+      border-bottom: 1px solid #ddd;
+    }
+
+    #myInput:focus {outline: 3px solid #ddd;}
+
+    .dropdown {
+      position: relative;
+      display: inline-block;
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      background-color: #f6f6f6;
+      min-width: 230px;
+      overflow: auto;
+      border: 1px solid #ddd;
+      z-index: 1;
+    }
+
+    .dropdown-content a {
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .dropdown a:hover {background-color: #ddd;}
+
+    .show {display: block;}
+    </style>
+
+
+ */
 
