0c474ba4a99c994a0d52aa78648a1edee494fa03
yvesshum <yvesshum1210@gmail.com>
2019-07-08 23:13:47 -0500

    edited example.vue

--- blackstone-webapp/src/views/youth/Example.vue
+++ blackstone-webapp/src/views/youth/Example.vue
@@ -1,16 +1,28 @@
 <template>
     <div class="example">
-        <p>here's an example display coming out from Example.vue, check out the code to see how this is imported here</p>
+        <p>Here's an example display coming out from Example.vue, check out the code to see how this is imported here</p><br><br>
+        <HelloWorld
+           @hi="myFunc()"
+        />
     </div>
-
 </template>
 
 <script>
+    import HelloWorld from "../../components/HelloWorld";
     export default {
         name: 'Example',
         components: {
+            HelloWorld
         },
-        methods: {
+        methods:{
+            myFunc() {
+                this.$emit('hi', object)
+            }
+        },
+        data() {
+            return {
+                myVariable: 0,
+            }
         }
     }
 </script>
