7725e8f3e00db4ac893c4f0c129e7910f3db4569
Yves Shum <yvesshum@mew.cs.uchicago.edu>
2019-07-10 21:09:00 -0500

    fixing example.vue and .gitignore

--- blackstone-webapp/src/views/youth/Example.vue
+++ blackstone-webapp/src/views/youth/Example.vue
@@ -2,7 +2,6 @@
     <div class="example">
         <p>Here's an example display coming out from Example.vue, check out the code to see how this is imported here</p><br><br>
         <HelloWorld
-           @hi="myFunc()"
         />
     </div>
 </template>
@@ -15,13 +14,9 @@
             HelloWorld
         },
         methods:{
-            myFunc() {
-                this.$emit('hi', object)
-            }
         },
         data() {
             return {
-                myVariable: 0,
             }
         }
     }
