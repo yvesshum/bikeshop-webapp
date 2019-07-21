5502c29e4c1e836da2cad8d2cecb075f842b23e6
jeffreymzhou <jmzhou@uchicago.edu>
2019-07-21 17:44:56 -0500

    init

---
+++ blackstone-webapp/src/plugin.js
@@ -0,0 +1,47 @@
+import {db} from '../../firebase';
+import {firebase} from '../../firebase';
+export default {
+  install (Vue, options) {
+    // Vue.prototype.$firestore_array_insert = (collection, doc, arrayName, object) => {
+    //   var objectString = this.$object_to_string(object)
+    //   var docRef = db.collection(collection).doc(doc)
+    //   var docData = docRef.data()
+    //   var array = docData[arrayName]
+    //   var objectKeysRef 
+    //   for(var i = 0; i < array.length; i++) {
+    //     if (array[i] !== 'deleted-object') {
+    //       objectKeysRef = array[i]
+    //       break
+    //     }
+    //   }
+    //   var existingKeys = this.$objectString_keys(objectKeysRef).sort()
+    //   var newObjectKeys = this.$objectString_keys(objectString).sort()
+    //   if (Array.prototype.equals(existingKeys, newObjectKeys)) {
+    //     docRef.update({
+    //       arrayName: firebase.firestore.FieldValue.arrayUnion(objectString)
+    //     }).then(response => {
+    //       return response
+    //     })
+    //   } else {
+    //     return 'error'
+    //   }
+      Vue.prototype.$plugin_test = () => {
+        console.log('hi')
+      }
+
+    }
+    // Vue.prototype.$firestore_array_remove = (object) => {
+
+    // }
+    // Vue.prototype.$firestore_array_update = (object) => {
+
+    // }
+    // Vue.prototype.$object_to_string = (object) => {
+    //   return object
+    // }
+    // Vue.prototype.$objectString_keys = (objectString) => {
+
+    // }
+
+  }
+}
