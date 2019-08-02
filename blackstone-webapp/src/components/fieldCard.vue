7d5836ea853f75301591b3b386bd28af6b4c89ad
yvesshum <yvesshum1210@gmail.com>
2019-08-02 09:54:07 -0500

    progress

---
+++ blackstone-webapp/src/components/fieldCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-
+        <p v-bind="field"></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fieldCard',
+    props: {
+        field: String
+    },
+}
+</script>
+
+<style>
+
+</style>
