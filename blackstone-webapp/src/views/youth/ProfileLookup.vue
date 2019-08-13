654c4b229c19c74513b79cf1098bf00b88b4f536
Vince <vincent.lagrassa@gmail.com>
2019-08-13 04:35:15 -0400

    Catch youth view up to staff view

--- blackstone-webapp/src/views/youth/ProfileLookup.vue
+++ blackstone-webapp/src/views/youth/ProfileLookup.vue
@@ -2,22 +2,38 @@
   <div class="profile_lookup_youth">
     <TopBar/>
     <p>This is the youth profile lookup page</p>
-    <!-- <YouthIDSelector /> -->
-    <ProfileFields />
-    <ApronBar />
-    <ProfileItemLogs />
+
+    <YouthIDSelector @selected="selectedID"/>
+
+    <div ref="body_fields" style="display: none;">
+      <ProfileFields :current-profile="currentProfile" :header_doc="this.header_doc" :allow_edits="false" :show_optional_fields="false" :hide_fields="hidden_fields" />
+
+      <!-- <ApronBar /> -->
+
+      <br /><br />
+
+      <h2>Order Log</h2>
+      <CollectionTable ref="order_log" :heading_data="this.order_log_headers" :current_collection="order_log_collection"></CollectionTable>
+
+      <br />
+
+      <h2>Work Log</h2>
+      <CollectionTable ref="work_log" :heading_data="this.work_log_headers" :current_collection="work_log_collection"></CollectionTable>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import {db} from '../../firebase';
+import {firebase} from '../../firebase';
 import firebase_app from 'firebase/app';
 import firebase_auth from 'firebase/auth';
 import TopBar from '@/components/TopBar';
 import YouthIDSelector from "@/components/YouthIDSelector.vue"
 import ProfileFields from "@/components/ProfileFields.vue"
 import ApronBar from "@/components/ApronBar.vue"
-import ProfileItemLogs from "@/components/ProfileItemLogs.vue"
+import CollectionTable from "@/components/CollectionTable.vue"
 
 export default {
   name: 'profile_lookup_youth',
@@ -26,10 +42,61 @@ export default {
     YouthIDSelector,
     ProfileFields,
     ApronBar,
-    ProfileItemLogs
+    CollectionTable
   },
-    methods: {
-      
-    }
+
+  data: function() {
+    return {
+      currentProfile: null,
+      order_log_collection: null,
+      work_log_collection: null,
+      header_doc: null,
+
+      log_headers_doc: null,
+
+      order_log_headers: [],
+      work_log_headers: [],
+
+      hidden_fields: ["Demerits"],
+    };
+  },
+
+  mounted: async function() {
+    this.header_doc = await db.collection("GlobalFieldsCollection").doc("Youth Profile").get();
+    this.log_headers_doc = await db.collection("GlobalFieldsCollection").doc("Log Table Headers").get();
+
+    this.order_log_headers = this.log_headers_doc.data()['Order Log Headers'];
+    this.work_log_headers = this.log_headers_doc.data()['Work Log Headers'];
+  },
+
+  methods: {
+
+    selectedID: async function(id) {
+
+      // No id returned - clear the page
+      if (id == null) {
+        this.load_none();
+      }
+
+      // Id returned - load profile for that youth
+      else {
+        this.$refs.body_fields.style.display = "";
+
+        id = id.slice(id.lastIndexOf(' ')+1);
+        let snapshot = db.collection("GlobalYouthProfile").doc(id);
+
+        this.currentProfile = await snapshot.get();
+        this.order_log_collection = snapshot.collection("Order Log");
+        this.work_log_collection  = snapshot.collection("Work Log");
+      }
+    },
+
+    load_none: function() {
+      this.$refs.body_fields.style.display = "none";
+      this.currentProfile = null;
+      this.order_log_collection = null;
+      this.work_log_collection  = null;
+    },
+  }
 }
 </script>
