fca8627e9f201a4fd80f0fa5a06f1987cb78bcbb
yvesshum <yvesshum1210@gmail.com>
2019-08-23 16:14:30 -0500

    stats progress

--- blackstone-webapp/src/views/staff/Stats.vue
+++ blackstone-webapp/src/views/staff/Stats.vue
@@ -1,10 +1,45 @@
 <template>
-    <top-bar/>
+    <div>
+        <top-bar/>
+        <b-container>
+            <b-row><b-col><h1>Stats</h1></b-col></b-row>
+            <b-row><b-col><h3>Attendance By Day</h3></b-col></b-row>
+            <b-row>
+                <b-col><DatePicker :lang="lang" v-model="date"/></b-col>
+                <b-col><b-button @click="handleSearchButtonClicked">Search</b-button></b-col>
+            </b-row>
+        </b-container>
+    </div>
+    
 </template>
 
 <script>
+import DatePicker from 'vue2-datepicker'
+import moment from 'moment'
 export default {
     name: 'Stats',
+    components: {
+        DatePicker,
+    },
+    data() {
+        return {
+            lang: {
+                days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                pickers: ['next 7 days', 'next 30 days', 'previous 7 days', 'previous 30 days'],
+                placeholder: {
+                    date: moment().format("YYYY-MM-DD"),
+                    dateRange: 'Select Date Range'
+                }
+            },
+            date: moment().format("YYYY-MM-DD")
+        }
+    },
+    methods: {
+        handleSearchButtonClicked() {
+            console.log(this.date);
+        }
+    }
     
 }
 </script>
