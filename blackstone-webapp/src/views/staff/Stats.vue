ab196c6a872dca245c3c10364ac12ec2b4942222
yvesshum <yvesshum1210@gmail.com>
2019-08-25 03:24:26 -0500

    Created SpecialInput component, demo in /staff/Stats.vue

--- blackstone-webapp/src/views/staff/Stats.vue
+++ blackstone-webapp/src/views/staff/Stats.vue
@@ -9,6 +9,47 @@
                 <b-col><b-button @click="handleSearchButtonClicked">Search</b-button></b-col>
             </b-row>
         </b-container>
+        <div class="Special-input-demo">
+            <p>Integer</p>
+            <SpecialInput input="Integer" ref='test2' :arguments="arguments1" />
+            <br>
+
+            <p>Boolean</p>
+            <SpecialInput input="Boolean" ref="test" :arguments="arguments1" />
+            <br>
+
+            <p>Phone</p>
+            <SpecialInput input="Phone" ref="phone" :arguments="{}"/>
+            <br>
+
+            <p>Date</p>
+            <SpecialInput input="Date" ref="date" :arguments="{}"/>
+            <br>
+
+            <p>Time</p>
+            <SpecialInput input="Time" ref="time"  :arguments="{}"/>
+            <br>
+
+            <p>Gender</p>
+            <SpecialInput input="Gender" ref="gender"  :arguments="{}"/>
+            <br>
+
+            <p>Race</p>
+            <SpecialInput input="Race" ref="race"  :arguments="{}"/>
+            <br>
+
+            <p>Grade</p>
+            <SpecialInput input="Grade" ref="race"  :arguments="{}"/>
+            <br>
+
+            <p>Email</p>
+            <SpecialInput input="Email" ref="race"  :arguments="{}"/>
+            <br>
+
+            <p>Anything else</p>
+            <SpecialInput input="someweirdtext" ref="race"  :arguments="{}"/>
+            <br>
+        </div>
     </div>
     
 </template>
@@ -16,10 +57,13 @@
 <script>
 import DatePicker from 'vue2-datepicker'
 import moment from 'moment'
+import SpecialInput from '../../components/SpecialInput.vue'
+
 export default {
     name: 'Stats',
     components: {
         DatePicker,
+        SpecialInput
     },
     data() {
         return {
@@ -32,14 +76,32 @@ export default {
                     dateRange: 'Select Date Range'
                 }
             },
-            date: moment().format("YYYY-MM-DD")
+            date: moment().format("YYYY-MM-DD"),
+            arguments1: {
+                "placeholder": "0",
+            }
         }
     },
     methods: {
         handleSearchButtonClicked() {
             console.log(this.date);
+            console.log('phone:', typeof(this.$refs.phone.get()))
         }
+    },
+
+    mounted() {
+        console.log('type:', typeof(this.$refs.test2.get()))
+        
+
     }
     
 }
 </script>
+
+<style>
+.Special-input-demo {
+    width: 50rem;
+    margin: 0 auto;
+
+}
+</style>
