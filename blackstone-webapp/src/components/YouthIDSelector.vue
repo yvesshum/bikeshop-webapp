bcd2ee37eb8a9a26798205f0f36a78bfe6491b88
Vince <vincent.lagrassa@gmail.com>
2019-09-09 12:06:53 -0400

    Create an actual search function

--- blackstone-webapp/src/components/YouthIDSelector.vue
+++ blackstone-webapp/src/components/YouthIDSelector.vue
@@ -39,7 +39,17 @@ Emits:
 
 <template>
     <div class = "YouthIDSelector">
-        <multiselect v-model="value" id="multiselect" :options="options" :placeholder="placeholder" open-direction="bottom" label="name" :custom-label="nameWithID">
+        <multiselect
+            v-model="value"
+            id="multiselect"
+            :options="filtered_options"
+            :placeholder="placeholder"
+            open-direction="bottom"
+            label="name"
+            :custom-label="nameWithID"
+            :internal-search="false"
+            @search-change="val => search_term = val"
+        >
             <template slot="singleLabel" slot-scope="props">
                 <span class="option__desc">
                     <span class="option__name">
@@ -88,16 +98,93 @@ Emits:
             return {
                 value: '',
                 options: [],
+                search_term: "",
 
                 vars_coll: db.collection("GlobalVariables"),
                 active_periods_doc: null,
                 past_periods_doc: null,
 
                 using_past: false,
-
-                id_list: null,
             }
         },
+
+        computed: {
+            /* Search algorithm:
+
+             A profile is considered to match the current search if there is a unique mapping from terms in the search (strings separated by spaces) to fields in the profile (First Name, Last Name, and ID).  That is, we want:
+
+             - Every term matches at least one field (e.g. no extra terms that match nothing)
+
+             - A unique mapping from terms to fields (e.g. if the user types in the first name five times, we don't want to match).
+
+             Special case: A blank search term is the same as no search term, not a search for an empty string (which, in fairness, would technically match everything).
+
+             */
+            filtered_options: function() {
+
+                // Special case: If the search term is blank, match everything
+                if (this.search_term == "") return this.options;
+
+                // Split search string into individual lowercase terms
+                var terms = this.search_term.split(' ').filter(s => s.length > 0);
+                var num_terms = terms.length;
+
+                // Filter the options
+                return this.options.filter(opt => {
+
+                    // Split fields into individual terms
+                    // This will catch name fields with multiple names, such as middle names (e.g. "John Smith Jones"
+                    var fields = [];
+                    Object.keys(opt).forEach(key => {
+                        opt[key].split(' ').filter(s => s.length > 0).forEach(s => fields.push(s));
+                    });
+
+                    // If the number of terms exceeds the number of fields, no match
+                    if (num_terms > fields.length) {
+                        return false;
+                    };
+
+                    // Create a matrix of Booleans representing which terms match which fields
+                    // Rows are fields, columns are terms
+                    let matched_fields = match_matrix(fields, terms, (field, term) => {
+
+                        // Get the name in lowercase as-is
+                        let normal = field.toLowerCase();
+
+                        // Get the name with all accents removed
+                        let plain = this.plaintext(normal);
+
+                        // Return true if the current term is a substring of either
+                        return normal.indexOf(term) !== -1 || plain.indexOf(term) !== -1;
+                    });
+
+                    // Calculate how many of the fields are matched by at least one term
+                    let num_fields_matched = matched_fields.reduce(
+                        // Increment the accumulator for each matrix row that has at least one "true" value
+                        (acc, curr) => acc += curr.includes(true) ? 1 : 0, 0
+                    );
+
+                    // Create an array of terms which don't match any field
+                    let unmatched_terms = terms.filter((term, n) => {
+
+                        // Grab each matrix row's value at the current term's index (e.g. whether the field for that row matches the current term) and combine all these values with OR (such that if any value in this term's column is true, the result will be true). Then, return it inverted for filter.
+                        // Equivalent to "false || matched_fields[0][n] || matched_fields[1][n] || ..."
+                        return !matched_fields.reduce((acc, curr) => acc || curr[n], false);
+                    });
+
+                    // The current option should match the search if:
+                    //  - There are no terms which don't match any field
+                    //  - The total number of terms is not greater than than the number of fields with a matching term. If it were greater, then two terms would have to match the same field.
+                    return (unmatched_terms.length === 0) && (num_fields_matched >= num_terms);
+                });
+
+                // Convert two arrays (rows, cols) into a matrix of booleans, where each cell represents whether the given row value and column value match according to some specified matching function (match).
+                function match_matrix(rows, cols, match) {
+                    return rows.map(row => cols.map(col => match(row, col)));
+                };
+            }
+        },
+
         methods: {
             // Retrieve all youth from the specified periods from the database
             async getData() {
@@ -185,19 +272,21 @@ Emits:
                 };
             },
 
-            // Package name and ID into a searchable string
-            // Includes name as-is and name with accents removed, for searching purposes
+            // Package name and ID into a string
             nameWithID (youth) {
-                let full_str = `${youth["First Name"]} ${youth["Last Name"]} ${youth["ID"]}`;
-                return full_str + " " + full_str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-                // https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+                return `${youth["First Name"]} ${youth["Last Name"]} ${youth["ID"]}`;
             },
 
             //Needed for Check In
             reset() {
                 this.value = null;
-            }
+            },
 
+            // Get a name string in plaintext (accents removed, etc)
+            // Source: https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript
+            plaintext(str) {
+                return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            },
         },
 
         watch: {
