1c12a7d8088a747224510c7917f1f1bd037ea769
Vince <vincent.lagrassa@gmail.com>
2019-09-22 14:22:23 -0400

    Create apron progress bar component

---
+++ blackstone-webapp/src/components/ApronProgressBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="apron_progress_bar">
+    <ApronImg 
+      v-for="(apron, n) in colors" style="display:inline-block;"
+      :color="apron.color" :size="size" :active="apron_active(n)"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ApronImg from '@/components/ApronImg';
+
+export default {
+  name: 'apron_progress_bar',
+
+  components: {ApronImg},
+
+  props: {
+    colors: {
+      type: Array,
+      // validator: function(arr) {
+      //   return arr.reduce((acc, curr) => acc && )
+      // },
+    },
+    size: {
+      type: Number,
+      default: 32,
+      validator: num => num > 0,
+    },
+    level: {
+      type: Number,
+      default: -1,
+    },
+  },
+
+  data: function() {
+    return {};
+  },
+
+  computed: {
+    apron_color: function() {
+      if (this.level != null && this.level != -1) {
+        return this.colors[this.level].name;
+      }
+      else {
+        return "";
+      };
+    },
+  },
+
+  methods: {
+    apron_active: function(n) {
+      return (this.level !== null && n <= this.level);
+    },
+  },
+}
+</script>
