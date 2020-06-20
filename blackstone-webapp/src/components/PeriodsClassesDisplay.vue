4dcd7b769514492128a8301a9481ec23a0e412a5
Vince <vincent.lagrassa@gmail.com>
2020-06-20 15:31:01 -0400

    Make UI friendlier
    Fixes #67

--- blackstone-webapp/src/components/PeriodsClassesDisplay.vue
+++ blackstone-webapp/src/components/PeriodsClassesDisplay.vue
@@ -11,21 +11,26 @@
         <br />
 
         <div v-if="detail_view != undefined">
+            <br />
             <table style="width: 95%; margin: auto; text-align:left;">
                 <tr>
                     <td style="width:5%;">{{ is_active(cur_period) ? "&#9745;" : "&#9744;" }}</td>
-                    <td style="width:47%;">Active in current quarter ({{cur_period}})</td>
-                    <td style="width:48%; text-align:center;">
+                    <td style="width:45%;">Active in current quarter ({{cur_period}})</td>
+                    <td style="width:5%;">{{ is_active(reg_period) ? "&#9745;" : "&#9744;" }}</td>
+                    <td style="width:45%;">Registered for next quarter ({{reg_period}})</td>
+                </tr>
+            </table>
+            <br />
+            </table>
+            <table style="width: 95%; margin: auto; text-align:left;">
+                <tr>
+                    <td style="width:35%; text-align:center;">
                         <span v-if="display.length == 0 || is_future_period(display)">
-                            <i>Choose a period below.</i>
+                            <i style="color: #999">Choose a period in the table below.</i>
                         </span>
                         <span v-else-if="is_active(display)">Youth's class in {{display}}:</span>
                         <span v-else><i>Youth was not active.</i></span>
                     </td>
-                </tr>
-                <tr>
-                    <td>{{ is_active(reg_period) ? "&#9745;" : "&#9744;" }}</td>
-                    <td>Registered for next quarter ({{reg_period}})</td>
                     <td>
                         <b-dropdown right id="dropdown-text" class="m-2"
                             :variant="dropdown_variant"
@@ -37,11 +42,14 @@
                                 <i>Change the status and class for {{youth_name}} in {{display}}.</i>
                             </b-dropdown-text>
                             <b-dropdown-divider></b-dropdown-divider>
-                            <b-dropdown-group id="add-class" header="Classes" style="width: 240px;">
+                            <b-dropdown-group id="add-class" header="Set Class" style="width: 240px;">
                                 <b-dropdown-item-button v-for="c in class_list" @click="set_class(display, c)">{{c}}</b-dropdown-item-button>
                             </b-dropdown-group>
                             <b-dropdown-divider></b-dropdown-divider>
-                            <b-dropdown-item-button @click="set_class(display, null)">-none-</b-dropdown-item-button>
+                            <b-dropdown-group id="rem-class" header="Remove Class" style="width: 240px;">
+                                <b-dropdown-item-button @click="set_class(display, 'Unknown')">Unknown</b-dropdown-item-button>
+                                <b-dropdown-item-button @click="set_class(display, null)">-none-</b-dropdown-item-button>
+                            </b-dropdown-group>
                         </b-dropdown>
                     </td>
                 </tr>
