c220ebb85fd7b0eb288f489c228c04bb398c1718
yvesshum <yvesshum1210@gmail.com>
2020-06-20 15:59:56 -0500

    Home

--- blackstone-webapp/src/views/Home.vue
+++ blackstone-webapp/src/views/Home.vue
@@ -122,7 +122,7 @@
                                 <button onclick="location.href='/youth-order-settings'" class="btn btn-outline-success btn-block" type="button">Youth Order Form</button>
                                 <button onclick="location.href='/youth-profile-staff-settings'" class="btn btn-outline-success btn-block" type="button">Youth Profile and Registration</button>
                                 <button onclick="location.href='/hour-logging-categories-settings'" class="btn btn-outline-success btn-block" type="button">Hour Logging Categories</button>
-                                <!-- <button onclick="location.href='/apron-colors-settings'" class="btn btn-outline-success btn-block" type="button">Apron Colors</button> -->
+                                <button onclick="location.href='/apron-colors-settings'" class="btn btn-outline-success btn-block" type="button">Apron Colors</button>
                                 <button onclick="location.href='/period-settings'" class="btn btn-outline-success btn-block" type="button">Period Settings</button>
 
                             </b-card-body>
