ce2c00927570f0025804629e8e05b38794e05282
yvesshum <yvesshum1210@gmail.com>
2020-08-29 18:15:58 -0500

    progress

--- blackstone-webapp/src/components/BlogPostContent.vue
+++ blackstone-webapp/src/components/BlogPostContent.vue
@@ -1,17 +1,17 @@
 <template>
-    <div id="blogPostContent">
-        <b-button variant="light" id="closeBtn" @click="closeHandler">
-            <font-awesome-icon icon="times" class="icon alt" size="lg" />
-        </b-button>
-        <p class="title">{{post.title}}</p>
-        <p class="subtitle">{{post.subtitle}}</p>
-        <div class="details">
-            <p class="posted">Posted {{posted}}</p>
-            <p class="posted">by {{post.posterName}}</p>
-        </div>
-
-        <p>{{post.content}}</p>
+  <div id="blogPostContent">
+    <b-button variant="light" id="closeBtn" @click="closeHandler">
+      <font-awesome-icon icon="times" class="icon alt" size="lg" />
+    </b-button>
+    <p class="title">{{ post.title }}</p>
+    <p class="subtitle">{{ post.subtitle }}</p>
+    <div class="details">
+      <p class="posted">Posted {{ posted }}</p>
+      <p class="posted">by {{ post.posterName }}</p>
     </div>
+
+    <p v-html="post.content"></p>
+  </div>
 </template>
 
 <script>
@@ -19,52 +19,52 @@ import { db } from "../../firebase";
 import PageHeader from "@/components/PageHeader.vue";
 import moment from "moment";
 export default {
-    name: "BlogPost",
-    props: {
-        post: Object,
-        closeHandler: Function,
-    },
+  name: "BlogPost",
+  props: {
+    post: Object,
+    closeHandler: Function,
+  },
 
-    computed: {
-        posted: function () {
-            return moment(this.post.time.toDate()).calendar();
-        },
+  computed: {
+    posted: function() {
+      return moment(this.post.time.toDate()).calendar();
     },
+  },
 
-    components: {
-        PageHeader,
-    },
+  components: {
+    PageHeader,
+  },
 
-    methods: {},
+  methods: {},
 };
 </script>
 
 <style scoped>
 .title {
-    font-size: 28pt;
-    font-weight: 500;
-    padding-left: 0;
+  font-size: 28pt;
+  font-weight: 500;
+  padding-left: 0;
 }
 .subtitle {
-    font-weight: 300;
-    font-size: 22pt;
+  font-weight: 300;
+  font-size: 22pt;
 }
 .posted {
-    font-size: 14pt;
+  font-size: 14pt;
 }
 .details {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 #blogPostContent {
-    padding: 2rem;
+  padding: 2rem;
 }
 
 #closeBtn {
-    position: absolute;
-    top: 5pt;
-    right: 5pt;
+  position: absolute;
+  top: 5pt;
+  right: 5pt;
 }
 </style>
