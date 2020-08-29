92cb4b340421f7d288d8312568417aab1201f0cb
Kate <kate755@gmail.com>
2020-08-29 16:06:57 -0700

    custom toolbar for rich text editor

--- blackstone-webapp/src/components/NewBlogPost.vue
+++ blackstone-webapp/src/components/NewBlogPost.vue
@@ -38,7 +38,7 @@
 
 
         <b-form-group id="input-group-4" label="Post Content:" label-for="input-4">
-					<vue-editor id="input-4" v-model="content"></vue-editor>
+					<RichTextEditor id="input-4"/>
 				</b-form-group>
         
 
@@ -50,7 +50,7 @@
 </template>
 
 <script>
-import { VueEditor } from "vue2-editor";
+import RichTextEditor from "@/components/RichTextEditor.vue";
 
 export default {
   name: "NewBlogPost",
@@ -59,11 +59,10 @@ export default {
         title: '',
         subtitle: '',
         name: '',
-        content: '<h1>Some initial content</h1>',
       }
   },
   components: {
-    VueEditor,
+    RichTextEditor,
   },
 	methods: {
 		onSubmit(evt) {
