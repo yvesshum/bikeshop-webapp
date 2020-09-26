f21fa71001258768e02edba2fa8b71b5deeef95f
yvesshum <yvesshum1210@gmail.com>
2020-09-27 02:17:58 +0800

    fixed scroll bar + styling

--- blackstone-webapp/src/components/NewBlogPost.vue
+++ blackstone-webapp/src/components/NewBlogPost.vue
@@ -1,53 +1,64 @@
 <template>
-    <b-container>
-        <b-modal v-model="showModal" size="lg" @hidden="closeModal" :hide-footer="true">
-            <div style="padding: 1rem 2rem;">
-                <b-card-title style="margin-bottom: 1rem">Create a blog post!</b-card-title>
-                <b-form @submit="onSubmit" @reset="onReset">
-                    <!-- Title -->
-                    <b-form-group id="input-group-1" label="Title:" label-for="input-1">
-                        <b-form-input
-                            id="input-1"
-                            v-model="blogTitle"
-                            required
-                            placeholder="Your title here"
-                        ></b-form-input>
-                    </b-form-group>
+  <b-modal
+    v-model="showModal"
+    size="lg"
+    @hidden="closeModal"
+    :hide-footer="true"
+  >
+    <div style="padding: 1rem 2rem;">
+      <b-card-title style="margin-bottom: 1rem"
+        >Create a blog post!</b-card-title
+      >
+      <b-form @submit="onSubmit" @reset="onReset">
+        <!-- Title -->
+        <b-form-group id="input-group-1" label="Title:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="blogTitle"
+            required
+            placeholder="Your title here"
+          ></b-form-input>
+        </b-form-group>
 
-                    <!-- Subtitle -->
-                    <b-form-group id="input-group-2" label="Subtitle:" label-for="input-2">
-                        <b-form-input
-                            id="input-2"
-                            v-model="blogSubtitle"
-                            required
-                            placeholder="Your subtitle here"
-                        ></b-form-input>
-                    </b-form-group>
+        <!-- Subtitle -->
+        <b-form-group id="input-group-2" label="Subtitle:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="blogSubtitle"
+            required
+            placeholder="Your subtitle here"
+          ></b-form-input>
+        </b-form-group>
 
-                    <!-- Name -->
-                    <b-form-group id="input-group-3" label="Your Name:" label-for="input-3">
-                        <b-form-input
-                            id="input-3"
-                            v-model="blogPosterName"
-                            required
-                            placeholder="Enter name"
-                        ></b-form-input>
-                    </b-form-group>
+        <!-- Name -->
+        <b-form-group id="input-group-3" label="Your Name:" label-for="input-3">
+          <b-form-input
+            id="input-3"
+            v-model="blogPosterName"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+        </b-form-group>
 
-                    <b-form-group id="input-group-4" label="Post Content:" label-for="input-4">
-                        <vue-editor
-                            v-model="blogContent"
-                            :editorToolbar="customToolbar"
-                            id="input-4"
-                        ></vue-editor>
-                    </b-form-group>
+        <b-form-group
+          id="input-group-4"
+          label="Post Content:"
+          label-for="input-4"
+        >
+          <vue-editor
+            v-model="blogContent"
+            :editorToolbar="customToolbar"
+            id="input-4"
+          ></vue-editor>
+        </b-form-group>
 
-                    <b-button type="submit" variant="success" style="margin-right: 12px">Submit</b-button>
-                    <b-button type="reset" variant="danger">Reset</b-button>
-                </b-form>
-            </div>
-        </b-modal>
-    </b-container>
+        <b-button type="submit" variant="success" style="margin-right: 12px"
+          >Submit</b-button
+        >
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
+  </b-modal>
 </template>
 
 <script>
@@ -56,99 +67,99 @@ import { VueEditor } from "vue2-editor";
 import { db } from "@/firebase.js";
 
 export default {
-    name: "NewBlogPost",
-    props: {
-        show: Boolean,
-        title: {
-            default: "",
-            type: String,
-        },
-        subtitle: {
-            default: "",
-            type: String,
-        },
-        name: {
-            default: "",
-            type: String,
-        },
-        content: {
-            default: "",
-            type: String,
-        },
-        submitCallback: {
-            default: () => {},
-            type: Function,
-        },
+  name: "NewBlogPost",
+  props: {
+    show: Boolean,
+    title: {
+      default: "",
+      type: String
     },
-    data() {
-        return {
-            showModal: false,
-            blogTitle: this.title,
-            blogSubtitle: this.subtitle,
-            blogPosterName: this.name,
-            blogContent: this.content,
-            customToolbar: [
-                [{ header: [false, 1, 2, 3, 4, 5, 6] }],
-                ["bold", "italic", "underline"],
-                ["blockquote"],
-                ["link"],
-                [{ align: "" }, { align: "center" }, { align: "right" }],
-                [{ font: [] }],
-                [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
-                [{ indent: "-1" }, { indent: "+1" }],
-                [{ color: [] }, { background: [] }],
-                ["clean"],
-            ],
-        };
+    subtitle: {
+      default: "",
+      type: String
     },
-    components: {
-        VueEditor,
+    name: {
+      default: "",
+      type: String
     },
-    methods: {
-        closeModal() {
-            this.$emit("close");
-        },
-        async onSubmit(evt) {
-            evt.preventDefault();
-            this.submitCallback({
-                content: this.blogContent,
-                posterName: this.blogPosterName,
-                subtitle: this.blogSubtitle,
-                title: this.blogTitle,
-            });
-            this.onReset(evt)
-        },
+    content: {
+      default: "",
+      type: String
+    },
+    submitCallback: {
+      default: () => {},
+      type: Function
+    }
+  },
+  data() {
+    return {
+      showModal: false,
+      blogTitle: this.title,
+      blogSubtitle: this.subtitle,
+      blogPosterName: this.name,
+      blogContent: this.content,
+      customToolbar: [
+        [{ header: [false, 1, 2, 3, 4, 5, 6] }],
+        ["bold", "italic", "underline"],
+        ["blockquote"],
+        ["link"],
+        [{ align: "" }, { align: "center" }, { align: "right" }],
+        [{ font: [] }],
+        [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ color: [] }, { background: [] }],
+        ["clean"]
+      ]
+    };
+  },
+  components: {
+    VueEditor
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+    async onSubmit(evt) {
+      evt.preventDefault();
+      this.submitCallback({
+        content: this.blogContent,
+        posterName: this.blogPosterName,
+        subtitle: this.blogSubtitle,
+        title: this.blogTitle
+      });
+      this.onReset(evt);
+    },
 
-        onReset(evt) {
-            evt.preventDefault();
-            this.blogTitle = "";
-            this.blogSubtitle = "";
-            this.blogPosterName = "";
-            this.blogContent = "";
-        },
+    onReset(evt) {
+      evt.preventDefault();
+      this.blogTitle = "";
+      this.blogSubtitle = "";
+      this.blogPosterName = "";
+      this.blogContent = "";
+    },
 
-        closeMsgModal() {
-            this.modal.msg.visible = false;
-        },
-    },
-    watch: {
-        show: function (newVal) {
-            if (newVal) {
-                this.showModal = true;
-            } else {
-                this.showModal = false;
-            }
-        },
-    },
+    closeMsgModal() {
+      this.modal.msg.visible = false;
+    }
+  },
+  watch: {
+    show: function(newVal) {
+      if (newVal) {
+        this.showModal = true;
+      } else {
+        this.showModal = false;
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
 .form-group {
-    margin-bottom: 1.25rem;
+  margin-bottom: 1.25rem;
 }
 
 ::v-deep label {
-    margin-bottom: 5px;
+  margin-bottom: 5px;
 }
 </style>
